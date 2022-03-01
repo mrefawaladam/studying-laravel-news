b47e9848e0bec34bb4af2181da5f489e5405f905
mrefawaladam <aerfa55@gmail.com>
2022-03-01 20:52:30 +0700

    setting routing

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\AboutUsController;
 use App\Http\Controllers\CategoryController; 
 use App\Http\Controllers\NewsController; 
 use App\Http\Controllers\ProgramController; 
+use App\Http\Controllers\ContactController; 
+
 
 
 
@@ -20,7 +22,26 @@ use App\Http\Controllers\ProgramController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
 // public index
 Route::get('/',[HomeController::class,'index'])->name('home');
  
- 
+// products
+Route::get('/category/marbel-edu-games',[CategoryController::class,'categoryOne']);
+Route::get('/category/marbel-and-friends-kids-games',[CategoryController::class,'categoryTwo']);
+Route::get('/category/riri-story-books',[CategoryController::class,'categoryTree']);
+Route::get('/category/kolak-kids-songs',[CategoryController::class,'categoryFour']);
+// news
+Route::get('/news',[NewsController::class,'index']);
+Route::get('/news/{keyword}',[NewsController::class,'newsKeyword']);
+// program
+Route::get('/program/karir',[ProgramController::class,'karir']);
+Route::get('/program/magang',[ProgramController::class,'magang']);
+Route::get('/program/kunjungan-industri',[ProgramController::class,'kunjunganIndustri']);
+// about us
+Route::get('/about-us',[AboutUsController::class,'index']);
+// contact us
+Route::get('/about-us',[ContactController::class,'index']);
+
+
+
