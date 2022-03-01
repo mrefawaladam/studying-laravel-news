92388aebd33f99b937f6f2d2db300224aca7b283
mrefawaladam <aerfa55@gmail.com>
2022-03-01 21:40:22 +0700

    changes navigation link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,31 +48,26 @@
 
       <nav id="navbar" class="navbar order-last order-lg-0">
         <ul>
-          <li><a class="active" href="index.html">Home</a></li>
-          <li><a href="about.html">About</a></li>
-          <li><a href="courses.html">Courses</a></li>
-          <li><a href="trainers.html">Trainers</a></li>
-          <li><a href="events.html">Events</a></li>
-          <li><a href="pricing.html">Pricing</a></li>
-
-          <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
+          <li><a class="/active" href="/">Home</a></li>
+          <li><a href="/about-us">About</a></li> 
+          <li class="dropdown"><a href="#"><span>Category</span> <i class="bi bi-chevron-down"></i></a>
             <ul>
-              <li><a href="#">Drop Down 1</a></li>
-              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
-                <ul>
-                  <li><a href="#">Deep Drop Down 1</a></li>
-                  <li><a href="#">Deep Drop Down 2</a></li>
-                  <li><a href="#">Deep Drop Down 3</a></li>
-                  <li><a href="#">Deep Drop Down 4</a></li>
-                  <li><a href="#">Deep Drop Down 5</a></li>
-                </ul>
-              </li>
-              <li><a href="#">Drop Down 2</a></li>
-              <li><a href="#">Drop Down 3</a></li>
-              <li><a href="#">Drop Down 4</a></li>
+              <li><a href="/category/marbel-edu-games">marbel-edu-games</a></li> 
+              <li><a href="/category/marbel-and-friends-kids-games">marbel-and-friends-kids-games</a></li>
+              <li><a href="/category/riri-story-books">riri-story-books</a></li>
+              <li><a href="/category/kolak-kids-songs">kolak-kids-songs</a></li>
             </ul>
           </li>
-          <li><a href="contact.html">Contact</a></li>
+          <li><a href="news">News</a></li>
+          <li class="dropdown"><a href="#"><span>Program</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="/program/karir">Karir</a></li> 
+              <li><a href="/program/magang">Magang</a></li>
+              <li><a href="/program/kunjungan-industri">Kunjungan Industri</a></li>
+              
+            </ul>
+          </li>
+          <li><a href="/contact-us'">Contact</a></li>
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
       </nav><!-- .navbar -->
